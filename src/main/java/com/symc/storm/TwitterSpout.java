8c3856d5eb496f4ba864223558151703e4d52d35
awesome <awesome@awesome-virtual-machine.(none)>
2013-07-20 14:59:31 -0400

    Hopefully spits out tuples

--- src/main/java/com/symc/storm/TwitterSpout.java
+++ src/main/java/com/symc/storm/TwitterSpout.java
@@ -1,5 +1,3 @@
-
-
 package com.symc.storm;
 
 import backtype.storm.Config;
@@ -22,95 +20,97 @@ import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
 
 public class TwitterSpout extends BaseRichSpout {
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	SpoutOutputCollector _collector;
-    LinkedBlockingQueue<Status> queue = null;
-    TwitterStream _twitterStream;
-    String _username;
-    String _pwd;
-    
-    
-    public TwitterSpout(String username, String pwd) {
-        _username = username;
-        _pwd = pwd;
-    }
-    
-    @Override
-    public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
-        queue = new LinkedBlockingQueue<Status>(1000);
-        _collector = collector;
-        StatusListener listener = new StatusListener() {
+	LinkedBlockingQueue<Status> queue = null;
+	TwitterStream _twitterStream;
+	String _username;
+	String _pwd;
 
-            @Override
-            public void onStatus(Status status) {
-                queue.offer(status);
-            }
+	public TwitterSpout(String username, String pwd) {
+		_username = username;
+		_pwd = pwd;
+	}
 
-            @Override
-            public void onDeletionNotice(StatusDeletionNotice sdn) {
-            }
+	@Override
+	public void open(@SuppressWarnings("rawtypes") Map conf,
+			TopologyContext context, SpoutOutputCollector collector) {
+		queue = new LinkedBlockingQueue<Status>(1000);
+		_collector = collector;
+		StatusListener listener = new StatusListener() {
 
-            @Override
-            public void onTrackLimitationNotice(int i) {
-            }
+			@Override
+			public void onStatus(Status status) {
+				queue.offer(status);
+			}
 
-            @Override
-            public void onScrubGeo(long l, long l1) {
-            }
+			@Override
+			public void onDeletionNotice(StatusDeletionNotice sdn) {
+			}
 
-            @Override
-            public void onException(Exception e) {
-            }
+			@Override
+			public void onTrackLimitationNotice(int i) {
+			}
+
+			@Override
+			public void onScrubGeo(long l, long l1) {
+			}
+
+			@Override
+			public void onException(Exception e) {
+			}
 
 			@Override
 			public void onStallWarning(StallWarning arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-            
-        };
-        TwitterStreamFactory fact = new TwitterStreamFactory(new ConfigurationBuilder().setUser(_username).setPassword(_pwd).build());
-        _twitterStream = fact.getInstance();
-        _twitterStream.addListener(listener);
-        _twitterStream.sample();
-    }
 
-    @Override
-    public void nextTuple() {
-        Status ret = queue.poll();
-        if(ret==null) {
-            Utils.sleep(50);
-        } else {
-            _collector.emit(new Values(ret));
-        }
-    }
+		};
+		TwitterStreamFactory fact = new TwitterStreamFactory(
+				new ConfigurationBuilder().setUser(_username).setPassword(_pwd)
+						.build());
+		_twitterStream = fact.getInstance();
+		_twitterStream.addListener(listener);
+		_twitterStream.sample();
+	}
 
-    @Override
-    public void close() {
-        _twitterStream.shutdown();
-    }
+	@Override
+	public void nextTuple() {
+		Status ret = queue.poll();
+		if (ret == null) {
+			Utils.sleep(50);
+		} else {
+			_collector.emit(new Values(ret));
+		}
+	}
 
-    @Override
-    public Map<String, Object> getComponentConfiguration() {
-        Config ret = new Config();
-        ret.setMaxTaskParallelism(1);
-        return ret;
-    }    
+	@Override
+	public void close() {
+		_twitterStream.shutdown();
+	}
 
-    @Override
-    public void ack(Object id) {
-    }
+	@Override
+	public Map<String, Object> getComponentConfiguration() {
+		Config ret = new Config();
+		ret.setMaxTaskParallelism(1);
+		return ret;
+	}
 
-    @Override
-    public void fail(Object id) {
-    }
+	@Override
+	public void ack(Object id) {
+	}
 
-    @Override
-    public void declareOutputFields(OutputFieldsDeclarer declarer) {
-        declarer.declare(new Fields("tweet"));
-    }
-    
+	@Override
+	public void fail(Object id) {
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("tweet"));
+	}
+
 }
