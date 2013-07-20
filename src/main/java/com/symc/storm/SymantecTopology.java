6f83d1df3cc98e980b5462d6ed7074967dfdb852
awesome <awesome@awesome-virtual-machine.(none)>
2013-07-20 14:16:10 -0400

    moved stuff around hoping it'll run

---
+++ src/main/java/com/symc/storm/SymantecTopology.java
@@ -0,0 +1,71 @@
+package com.symc.storm;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.testing.TestWordSpout;
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SymantecTopology {
+
+
+    public static class HeatBolt extends BaseBasicBolt {
+
+        @Override
+        public void execute(Tuple tuple, BasicOutputCollector collector) {
+            String word = tuple.getString(0);
+
+            collector.emit(new Values(word));
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("location"));
+        }
+    }
+
+
+        public static void main(String[] args) throws Exception {
+           TopologyBuilder builder = new TopologyBuilder();
+
+           builder.setSpout("tweet", new TwitterSpout(args[0], args[1]), 10);
+           builder.setBolt("heat", new HeatBolt(), 3)
+                .fieldsGrouping("tweet", new Fields("location"));
+          Config conf = new Config();
+           conf.setDebug(true);
+
+          /* if(args!=null && args.length > 0) {
+             conf.setNumWorkers(3);
+
+              StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+           } else {
+	*/
+             LocalCluster cluster = new LocalCluster();
+             cluster.submitTopology("test", conf, builder.createTopology());
+             Utils.sleep(10000);
+             cluster.killTopology("test");
+             cluster.shutdown();
+        //}
+    }
+
+
+
+
+
+
+
+}
+
